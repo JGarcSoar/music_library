4b4154d0787824e3d4eab5bd740497b53062047d
JGarcSoar <garciasoares.joe@gmail.com>
2023-01-02 17:13:16 -0800

    using props and context

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import SearchBar from "./components/SearchBar";
 import AlbumView from "./components/AlbumView";
 import ArtistView from "./components/ArtistView";
 import { Fragment } from "react/cjs/react.production.min";
+import { DataContext } from "./context/DataContext";
 
 function App() {
   let [search, setSearch] = useState("");
@@ -44,7 +45,9 @@ function App() {
             element={
               <Fragment>
                 <SearchBar handleSearch={handleSearch} />
-                <Gallery data={data} />
+                <DataContext.Provider value={data}>
+                  <Gallery />
+                </DataContext.Provider>
               </Fragment>
             }
           />
